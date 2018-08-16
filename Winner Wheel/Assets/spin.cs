43ac13355c17f75e35904914f5d37035af88ff47
Fernando de Paz <fdepaz@uci.edu>
2018-08-16 09:51:27 -0700

    updated spinning Code

--- Winner Wheel/Assets/spin.cs
+++ Winner Wheel/Assets/spin.cs
@@ -8,33 +8,53 @@ public class spin : MonoBehaviour {
     public float max = 10;
     public bool rotating;
     public float randomTime;
+    public bool spinning;
+    public float currentSpeed;
+    private bool reductionReady;
 	// Use this for initialization
 	void Start () {
-		
+        currentSpeed = rotationSpeed;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetMouseButtonDown(0)) {
+        if (Input.GetMouseButtonDown(0) && !spinning) {
             rotating = !rotating;
             if (rotating == true) {
+                currentSpeed = rotationSpeed;
+                reductionReady = false;
+                
                 randomTime = Random.Range(min, max);
             }
-            
+            StartCoroutine(WheelWait());
         }
         if (rotating)
         {
-            Vector3 spin = new Vector3(0, rotationSpeed, 0);
-            transform.Rotate(spin * Time.deltaTime);
-            StartCoroutine(WheelWait());
+            if (currentSpeed > 0)
+            {
+                Vector3 spin = new Vector3(0, currentSpeed, 0);
+                transform.Rotate(spin * Time.deltaTime);
+                if (!reductionReady)
+                {
+                    StartCoroutine(speedReduction());
+                }
+            }
+            
         }
         
 
 	}
-
+    IEnumerator speedReduction() {
+        currentSpeed -= 10;
+        reductionReady = true;
+        yield return new WaitForSeconds(.200f);
+        reductionReady = false;
+        
+    }
     IEnumerator WheelWait() {
-
+        spinning = true;
         yield return new WaitForSeconds(randomTime);
         rotating = false;
+        spinning = false;
     }
 }
